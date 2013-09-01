97ddac2fd76067421df6afd5fab7c64bc15ed992
Marc S. Brooks <marc@mbrooks.info>
2013-09-01 04:53:30 -0700

    Added new class to provide server concurrency

---
+++ src/thread.java
@@ -0,0 +1,27 @@
+/**
+ *  tiny-http (thread.java)
+ *  A compact, multi-threaded, HTTP server written in Java
+ *
+ *  Copyright 2013, Marc S. Brooks (http://mbrooks.info)
+ */
+import java.util.concurrent.*;
+
+public class thread {
+	static int threads = 1;
+
+	/**
+	 * @param String[] args
+	 */
+	public static void main(String[] args) {
+		ExecutorService threadExec = Executors.newFixedThreadPool(threads);
+		
+		try {
+			server serverStart = new server();
+
+			threadExec.execute(serverStart);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+}
