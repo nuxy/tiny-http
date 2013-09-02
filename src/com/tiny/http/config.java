7e3f2de59963978d4f19b8561b3d1d9ac28cb46e
Marc S. Brooks <marc@mbrooks.info>
2013-09-02 02:30:58 -0700

    Refactored loadFile() method
    Added new methods for retrieving XML node(s) and values
    
    Updated package imports to be specific vs wildcarded

--- src/com/tiny/http/config.java
+++ src/com/tiny/http/config.java
@@ -6,30 +6,44 @@
  */
 package com.tiny.http;
 
-import java.io.*;
-import java.util.*;
-import javax.xml.parsers.*;
+import java.io.File;
+import java.util.Map;
+import java.util.HashMap;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.*;
 
 public class config {
+	private Document doc = null;
 
 	/**
-	 * Parse the XML configuration file
-	 * @param  String name
-	 * @return Map<String, String>
+	 * Load the XML configuration file
+	 * @param String name
 	 */
-	public Map<String, String> parseFile(String name) {
-		Map<String, String> elmMap = new HashMap<String, String>();
-
+	public void loadFile(String name) {
 		try {
 			File confFile = new File(name);
 
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder        dBuilder  = dbFactory.newDocumentBuilder();
-			Document               doc       = dBuilder.parse(confFile);
 
-			// convert child elements to hashmap
-			NodeList nodeList  = doc.getElementsByTagName("server");
+			doc = dBuilder.parse(confFile);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Get all options for a given group (XML parent element)
+	 * @param  String groupName
+	 * @return Map<String, String>
+	 */
+	public Map<String, String> getOptionsByGroup(String groupName) {
+		Map<String, String> elmMap = new HashMap<String, String>();
+
+		try {
+			NodeList nodeList  = doc.getElementsByTagName(groupName);
 			Node     rootElm   = nodeList.item(0);
 			NodeList childList = rootElm.getChildNodes();
 
@@ -46,5 +60,26 @@ public class config {
 		}
 
 		return elmMap;
+	}
+
+	/**
+	 * Get option value, by name, for a given group
+	 * @param  String groupName
+	 * @param  String optName
+	 * @return String value
+	 */
+	public String getOptionValByName(String groupName, String optName) {
+		Map<String, String> options = getOptionsByGroup(groupName);
+
+		String value = new String();
+
+		for (Map.Entry<String, String> entry : options.entrySet()) {
+			if (entry.getKey() == optName) {
+				value = entry.getValue();
+				break;
+			}
+		}
+
+		return value;
 	}
 }
